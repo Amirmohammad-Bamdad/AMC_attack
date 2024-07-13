3394d126479248696ccfbe477221d206668a0cde
amir <bamdad.amb@gmail.com>
2024-07-13 23:43:23 +0330

    FGSM is ready for multi-data test. also the test function and some other details related to testing are now enhanced

--- attack.py
+++ attack.py
@@ -14,7 +14,8 @@ def initialize_parameters():
     snrs = data.snrs
     labels = data.label
     test_indices = data.test_indices
-    x_val, y_val = data.val_data[0], data.val_data[1]
+
+    x_val, y_val = data.val_data[0], data.val_data[1] # Using val data or training purpose.(Because it is not too large like train set also has no overlap with test set)
     x_test, y_test = data.test_data[0], data.test_data[1]
 
     m = modelFile.CNNModel(input_shape=(2, 128), classes= len(mods))
@@ -39,10 +40,10 @@ def calculate_gradient(model, input_signal, input_label):
         loss = loss_func(input_label, y)
 
     gradient = tape.gradient(loss, input_signal)
-    
     return gradient
 
-def white_box_accuracy_test(Y, Y_adv_signal, input_label, classes):
+
+def single_sample_test(Y, Y_adv_signal, input_label, classes):
     class_Y = classes[int(np.argmax(Y, axis=1))]
     class_Y_adv_signal = classes[int(np.argmax(Y_adv_signal, axis=1))]
     class_input_label = classes[int(np.argmax(input_label, axis=1))]
@@ -53,19 +54,62 @@ def white_box_accuracy_test(Y, Y_adv_signal, input_label, classes):
     print("==================================================")
     
 
-def FGSM(model, epsilons, input_signal, input_label, classes):
+def adversary_test(model, r, data, labels, snrs, mods, test_indices, snr_labels, attack_name):
+    adv_data = data + r
+    test_loss, test_acc  = model.evaluate(data, labels, batch_size= 256)
+    adv_test_loss, adv_test_acc  = model.evaluate(adv_data, labels, batch_size= 256)
+    print("Test accuracy (Normal): ", test_acc)
+    print("Test loss (Normal): ", test_loss)
+    print("Test accuracy (Adversarial): ", adv_test_acc)
+    print("Test loss (Adversarial): ", adv_test_loss)
+    
+    proto_tensor = tf.make_tensor_proto(adv_data)
+    adv_data = tf.make_ndarray(proto_tensor)
+    print(adv_data.shape)
+    
+    _, acc_mod_snr, bers = utils.evaluate_per_snr(model= model, X_test= adv_data, Y_test= labels,
+                                           snrs= snrs, classes= mods, labels= snr_labels,
+                                             test_indices= test_indices)
+    
+    utils.plot_accuracy_per_snr(snrs= snrs, acc_mod_snr= acc_mod_snr, classes= mods, name= attack_name)
+    utils.plot_ber_vs_snr(snrs, bers, name= attack_name)
+
+
+def FGSM(model, epsilons, input_signal, input_label, x_test, y_test,
+          snrs, classes, test_indices, snr_labels):
+    '''
+        https://arxiv.org/pdf/1412.6572
+    '''
+    input_signal = tf.convert_to_tensor(input_signal)
+    input_label = tf.convert_to_tensor(input_label)
+    r = None
+    epsilon = None
+    
     for eps in epsilons:
         gradient = calculate_gradient(model, input_signal, input_label)
         perturbation = tf.sign(gradient)
         adv_signal = input_signal + eps*perturbation
         Y_adv_signal = model(adv_signal)
-
         Y = model.predict(input_signal)
-        print(f"epsilon = {eps}")
-        white_box_accuracy_test(Y, Y_adv_signal, input_label, classes)
+        
+        #print(f"epsilon = {eps}")
+        #single_sample_test(Y, Y_adv_signal, input_label, classes)
+        if (np.sum(Y == Y_adv_signal) > Y.shape[0]//2) or (eps == epsilons[-1]):
+            epsilon = eps
+            print(f"The best epsilon is: {eps}")
+            r= perturbation
+            break
+    
+    r = r*epsilon
+    adversary_test(model= model, r= r, data= x_test, labels= y_test,
+                    snrs= snrs, mods= classes, test_indices= test_indices,
+                      snr_labels= snr_labels, attack_name= "FGSM")
         
 
-def bisection_search_whitebox_attack(model, input_data, input_label, classes):
+def bisection_search_FGM(model, input_data, input_label, classes, snr_labels):
+    '''
+    https://arxiv.org/abs/1808.07713
+    '''
     eps_acc = 0.00001 * np.linalg.norm(input_data)  
     target_class_onehot = np.zeros([len(classes)])
     epsilon_vector = np.zeros([len(classes)])
@@ -87,7 +131,7 @@ def bisection_search_whitebox_attack(model, input_data, input_label, classes):
                 min_epsilon = avg_epsilon
             else:
                 max_epsilon = avg_epsilon
-            #print(adv_label)
+
         epsilon_vector[class_index] = max_epsilon
 
     target_class = np.argmin(epsilon_vector)
@@ -104,34 +148,16 @@ def bisection_search_whitebox_attack(model, input_data, input_label, classes):
     Y_adv_signal = model.predict(adv_input)
     Y = model.predict(input_data)
 
-    white_box_accuracy_test(Y, Y_adv_signal, input_label, classes)
+    #single_sample_test(Y, Y_adv_signal, input_label, classes)
+    adversary_test(model, Y_adv_signal, x_test, y_test, snrs, mods,
+                    test_indices, snr_labels, attack_name= "BiSearch_FGM")
 
 
-
-def black_box_attack_test(model, r, data, labels, snrs, mods, test_indices, snr_labels):
-    adv_data = data + r
-    test_loss, test_acc  = model.evaluate(data, labels, batch_size= 256)
-    adv_test_loss, adv_test_acc  = model.evaluate(adv_data, labels, batch_size= 256)
-    print("Test accuracy (Normal): ", test_acc)
-    print("Test loss (Normal): ", test_loss)
-    print("Test accuracy (Adversarial): ", adv_test_acc)
-    print("Test loss (Adversarial): ", adv_test_loss)
-    
-    print(data)
-    proto_tensor = tf.make_tensor_proto(adv_data)
-    adv_data = tf.make_ndarray(proto_tensor)
-
-    print(adv_data)
-    acc, acc_mod_snr, bers = utils.evaluate_per_snr(model= model, X_test= adv_data, Y_test= labels,
-                                           snrs= snrs, classes= mods, labels= snr_labels,
-                                             test_indices= test_indices)
-    
-    utils.plot_accuracy_per_snr(snrs= snrs, acc_mod_snr= acc_mod_snr, classes= mods, name= "UAP_PCA")
-    utils.plot_ber_vs_snr(snrs, bers, name= "UAP_PCA")
-
-
-def pca_based_black_box_attack(model, data_points, label_points, x_test, y_test,
+def uap_pca_attack(model, data_points, label_points, x_test, y_test,
                                              snrs, mods, test_indices, snr_labels):
+    '''
+    https://arxiv.org/abs/1808.07713
+    '''
     max_epsilon = np.linalg.norm(data_points)
     
     data_points = tf.convert_to_tensor(data_points)
@@ -154,7 +180,8 @@ def pca_based_black_box_attack(model, data_points, label_points, x_test, y_test,
 
     extended_UAP = tf.tile(UAP_r, [1,2,128])
 
-    black_box_attack_test(model, extended_UAP, x_test, y_test, snrs, mods, test_indices, snr_labels)
+    adversary_test(model, extended_UAP, x_test, y_test, snrs, mods,
+                    test_indices, snr_labels, attack_name= "UAP_PCA")
 
 
 
@@ -162,14 +189,11 @@ if __name__ == "__main__":
     model, loss_func, epsilons, snrs, x_test, y_test, x_val, y_val, mods, labels, test_indices = initialize_parameters()
     
     input_signal, input_label = sample_input(x_test, y_test)
-    
     reshaped_input = tf.expand_dims(input_signal, axis=0)
     reshaped_label = tf.expand_dims(input_label, axis=0)
 
     # White-Box Attack
-    #FGSM(model, epsilons, reshaped_input, reshaped_label, mods)
-    #bisection_search_whitebox_attack(model, reshaped_input, reshaped_label, mods)
-    
-    # BLack-Box Attack
-    pca_based_black_box_attack(model, x_val, y_val, x_test, y_test, snrs, mods, test_indices, labels)
+    #FGSM(model, epsilons, x_val, y_val, x_test, y_test, snrs, mods, test_indices, labels)
+    bisection_search_FGM(model, reshaped_input, reshaped_label, mods, labels)
+    #uap_pca_attack(model, x_val, y_val, x_test, y_test, snrs, mods, test_indices, labels)
     
